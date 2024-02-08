34538967be5e6d6c14e7872d8351bada7e5443f6
Diego de Silva Setién <dasiga97@gmail.com>
2024-02-08 12:09:35 +0100

    Follow camera
    Based on SpriteRender using x flip

---
+++ EdixJuego2D/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer target;
+    [Space]
+    [SerializeField] float speed = 1;
+    [SerializeField] float offsetHorizontal = 1.25f;
+    [SerializeField] float offsetVertical = 1.5f;
+    [SerializeField] float minDistanceStop = .5f;    
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(getTargetPosition(), transform.position) > minDistanceStop)
+            transform.position = Vector3.Lerp(transform.position, getTargetPosition() + getRelativePosition(), speed * Time.deltaTime);
+            
+    }
+
+    Vector3 getRelativePosition()
+    {
+        Vector3 offset = new Vector3(target.flipX ? -offsetHorizontal : offsetHorizontal, offsetVertical, 0);
+        return offset;
+    }
+
+    Vector3 getTargetPosition()
+    {
+        Vector3 pos = target.transform.position;
+        pos.z = transform.position.z;
+        return pos;
+    }
+
+}
