72059595cc3767885a42f018e6ddc6b834adefbb
Diego de Silva Setién <dasiga97@gmail.com>
2024-02-15 11:49:36 +0100

    Scenes & Fonts
    Comunication scenes and fonts added with some layout buttons added too

--- EdixJuego2D/Assets/Scripts/Controllers/Menus/MainMenu.cs
+++ EdixJuego2D/Assets/Scripts/Controllers/Menus/MainMenu.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Button playButton;
+    [SerializeField] Button optionsButton;
+    [SerializeField] Button exitButton;
 
     void Start()
     {
-        Invoke(nameof(HandlePlay), 1f);
+        playButton.onClick.AddListener(() => SceneManager.LoadScene(1));
+        optionsButton.onClick.AddListener(() => MenuHandler.changeMenu(FindObjectOfType<SettingsMenu>()));
+        exitButton.onClick.AddListener(() => Application.Quit());
     }
-
-    private void HandlePlay()
-    {
-        SceneManager.LoadScene(1);
-    }
-  
 }
