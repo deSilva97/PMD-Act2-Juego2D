df71ba112bf310f05ff2d596c5ac5ca6ef614211
Diego de Silva Setién <dasiga97@gmail.com>
2024-02-19 09:58:58 +0100

    override player hud
    Modify life bar to sprite hearths and change diamonds sprites.

--- EdixJuego2D/Assets/Scripts/Controllers/Items/KeyController.cs
+++ EdixJuego2D/Assets/Scripts/Controllers/Items/KeyController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class KeyController : Pickeable
 {
-
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         PickUp();
--- EdixJuego2D/Assets/Scripts/Controllers/UI/PlayerHUD.cs
+++ EdixJuego2D/Assets/Scripts/Controllers/UI/PlayerHUD.cs
@@ -8,50 +8,55 @@ using UnityEngine.UI;
 public class PlayerHUD : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] Image currentLife;
+    //[SerializeField] Image currentLife;
+    [SerializeField] Image[] hearths;
     [SerializeField] TextMeshProUGUI numberLifes;
     [SerializeField] TextMeshProUGUI points;
-    [SerializeField] Image key;
     [SerializeField] Image[] chests;
-    [Space]
-    [SerializeField] TextMeshProUGUI differentialPointsText;
 
     [Header("Params")]    
     [SerializeField][Range(0, 1)] float alphaChests = .2f;
-
+    [SerializeField] Sprite[] hearthSprites;
 
     private void OnEnable()
     {
-        PlayerController.onPlayerLifeChange += SetLifeBar;
+        PlayerController.onPlayerLifeChange += SetLifeSprite;
         PlayerManager.onExtraLifesChanges += UpdateTries;
         LevelManager.onPointAdded += SetPoints;
-        PlayerManager.onKeyPicked += ShowKey;
         PlayerManager.onChestOpened += SetChestAlpha;
     }
 
     private void OnDisable()
     {
-        PlayerController.onPlayerLifeChange -= SetLifeBar;
+        PlayerController.onPlayerLifeChange -= SetLifeSprite;
         PlayerManager.onExtraLifesChanges -= UpdateTries;
         LevelManager.onPointAdded -= SetPoints;
-        PlayerManager.onKeyPicked -= ShowKey;
         PlayerManager.onChestOpened -= SetChestAlpha;
     }
 
     private void Start()
-    {
-        differentialPointsText.text = "";
-
-        key.gameObject.SetActive(false);
-
+    {        
         foreach(Image i in chests)
             i.color = new Color(i.color.r, i.color.g, i.color.b, alphaChests);
     }
 
-    private void SetLifeBar(int current, int max)
+    private void SetLifeSprite(int current, int max)
     {
-        float dif = (float)current / max;
-        currentLife.fillAmount = dif;
+        foreach (Image img in hearths)
+            img.sprite = hearthSprites[0];
+
+        int indexHearth = 0;
+
+        for (int i = 1; i <= current; i++)
+        {
+            if(i % 2 == 0)
+            {
+                hearths[indexHearth].sprite = hearthSprites[1];
+                indexHearth++;
+            }
+            else hearths[indexHearth].sprite = hearthSprites[2];
+        }
+
     }
 
     private void SetPoints(int currentValue, int amount)
@@ -70,12 +75,6 @@ public class PlayerHUD : MonoBehaviour
             chests[i].color = new Color(chests[i].color.r, chests[i].color.g, chests[i].color.b, (i < number) ? 1: alphaChests);
         }
     }
-
-    private void ShowKey(bool value)
-    {
-        key.gameObject.SetActive(value);
-    }
-
     private void UpdateTries(int value)
     {
         numberLifes.text = "x" + value;
